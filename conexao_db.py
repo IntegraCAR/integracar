1e26b20f213717f26b0f4609c3ec858b93c48687
Beatriz Messias <beatrizmessiascr@gmail.com>
2025-10-21 15:43:30 -0300

    Adiciona conexão com o banco de dados

---
+++ conexao_db.py
@@ -0,0 +1,27 @@
+import psycopg2
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
+HOST = os.getenv("HOST")
+PORT = os.getenv("PORT")
+DBNAME = os.getenv("DBNAME")
+
+def criar_conexao():
+    try:
+        connection = psycopg2.connect(
+            user=USER,
+            password=PASSWORD,
+            host=HOST,
+            port=PORT,
+            dbname=DBNAME
+        )
+        print("Conexão realizada com sucesso!")
+        return connection
+    
+    except Exception as e:
+        print(f"Erro ao conectar ao banco: {e}")
+        return None
